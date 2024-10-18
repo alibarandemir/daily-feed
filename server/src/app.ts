89d2e00007e6037894cdd6eab358220908abd05e
Ali Baran Demir <alibarandemir798@gmail.com>
2024-10-18 12:23:30 +0300

    server added rate limit

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,22 @@
 import express, { Express,Application } from "express";
 import cors from 'cors'
 import router from "./routes/route";
+import rateLimit from "express-rate-limit";
 const app= express()
 app.use(express.json())
 
 app.use(cors({
     origin: '*', 
 }));
+
+const limiter= rateLimit({
+    windowMs:15*60*1000,
+    limit:50,
+    message:'Çok fazla istek yapıldı',
+    standardHeaders:true,
+    legacyHeaders:false
+})
+app.use(limiter)
 app.use('/',router)
 
 app.listen(5000,()=>{
