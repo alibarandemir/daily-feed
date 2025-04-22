991254d82c49dcb1a863e5d8066d419e2184da8c
Ali Baran Demir <alibarandemir798@gmail.com>
2025-04-22 19:51:19 +0300

    weather api url updated

--- client/src/config/axios.ts
+++ client/src/config/axios.ts
@@ -7,7 +7,7 @@ export const api= axios.create({
 }) 
 
 export const apiRoute= axios.create({
-    baseURL:'http://localhost:3000/api'
+    baseURL:'https://sumflood-client.onrender.com//api'
 })
 
 
