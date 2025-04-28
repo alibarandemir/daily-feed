4d9c545f30ae4c75a710c09257dd10412505a03a
Ali Baran Demir <alibarandemir798@gmail.com>
2025-04-28 20:56:45 +0300

    backend url updated for frontend

--- ScrapingService/obj/Debug/net8.0/ScrapingService.AssemblyInfo.cs
+++ ScrapingService/obj/Debug/net8.0/ScrapingService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScrapingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d381cb251e3b4e819180096835397744af7978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14e334da838d7cd3f88d2ef849abb9d102cff7da")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScrapingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScrapingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
