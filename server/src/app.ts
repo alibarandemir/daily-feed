f3b78dbe45bfba5f2eaeffea49e2af89e386d544
Ali Baran Demir <alibarandemir798@gmail.com>
2025-05-03 12:59:07 +0300

    dff

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,7 +18,7 @@ const app= express()
 app.use(express.json())
 
 app.use(cors({
-    origin: 'https://sumflood-client.onrender.com', 
+    origin: ['https://sumflood-client.onrender.com','http://localhost:3000'], 
     credentials: true,
     methods:'GET,POST,PUT,DELETE'
   }));
