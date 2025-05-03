d60fd93b41ea48090883ae64bb016b47884dd529
Ali Baran Demir <alibarandemir798@gmail.com>
2025-05-03 12:41:23 +0300

    cors origin

--- ScrapingService/obj/Debug/net8.0/ScrapingService.AssemblyInfo.cs
+++ ScrapingService/obj/Debug/net8.0/ScrapingService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScrapingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14e334da838d7cd3f88d2ef849abb9d102cff7da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d9c545f30ae4c75a710c09257dd10412505a03a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScrapingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScrapingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
