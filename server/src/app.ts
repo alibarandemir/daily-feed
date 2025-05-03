cb816d075d8e2e592e72f0a5978644bba2a7dea6
Ali Baran Demir <alibarandemir798@gmail.com>
2025-05-03 12:31:20 +0300

    cors origin updated

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,7 +18,7 @@ const app= express()
 app.use(express.json())
 
 app.use(cors({
-    origin: 'http://localhost:3000', 
+    origin: 'https://sumflood-client.onrender.com', 
     credentials: true,
     methods:'GET,POST,PUT,DELETE'
   }));
